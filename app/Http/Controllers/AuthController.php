13d32df2de78255a67c642f395a404acb08b830d
Benjamin Kan <kanjc123@gmail.com>
2026-07-01 18:22:53 +0800

    feat: auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        throw ValidationException::withMessages([
+            'password' => __('Invalid email or password.'),
+        ]);
+    }
+
+    public function register(Request $request): RedirectResponse
+    {
+        $user = app(CreateNewUser::class)->create($request->all());
+
+        Auth::login($user);
+        $request->session()->regenerate();
+
+        return redirect()->intended('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,10 +11,12 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('pages.auth.login');
 });
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 
 Route::get('/register', function () {
     return view('pages.auth.register');
 });
+Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
 // Assignments
 Route::get('/assignments', function () {
